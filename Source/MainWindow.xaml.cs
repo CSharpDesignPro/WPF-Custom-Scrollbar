978be0691e22105e39b470640ebde3fd4eae9212
Arun Mutharasu <55704859+CSharpDesignPro@users.noreply.github.com>
2020-03-23 13:24:56 +0530

    Add files via upload

---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿
+/// Summary /// 
+/// Author : R. Arun Mutharasu ///
+/// Created : 12-02-2020
+/// YouTube Channel : C# Design Pro ///
+/// Summary ///   
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Custom_Scrollbar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    /// <summary>
+    /// Create a Value Converter to disable the Up & Down Arrow buttons of the scrollbar
+    /// when the Thumb reaches the minimum & maximum position on the scroll track.
+    /// </summary>
+
+    public class ScrollLimitConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double && values[1] is double)
+            {
+                return (double)values[0] == (double)values[1];
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
